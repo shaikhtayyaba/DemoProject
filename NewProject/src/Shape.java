f5ebc945895705f0877d3d227a5f514a584d579b
tayyabashaikh <shaikhtayyaba25@gmail.com>
2022-11-15 16:23:53 +0530

    My 3rd commit

---
+++ NewProject/src/Shape.java
@@ -0,0 +1,25 @@
+
+    abstract class Shape {
+
+	             String color;
+			   
+			   //these are abstract method
+			   abstract double area();
+			   public abstract String toString();
+			   
+			   //abstract class can have the constructor
+			   public Shape(String color)
+			   {
+				   System.out.println("Shape constructor called");
+				   this.color=color;
+			   }
+			   
+			   //this is a concrete method
+			   public String getColor() {return color ;}
+		   
+			
+
+	}
+
+	
+	
